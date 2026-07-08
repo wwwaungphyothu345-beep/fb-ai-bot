5558e9c498a704e0ad672027e7d4576f0e7ada1b
Aung Phyo Thu <www.aungphyothu345@gmail.com>
2026-07-08 22:41:33 +0630

    Fix indentation spaces

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def webhook():
                     
     return Response(status=200)
 
-if__name__=='__main__':
-app.run(host='0.0.0.0',port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
