cb120aac799eced18e295d82c39841e90ba7383d
Aung Phyo Thu <www.aungphyothu345@gmail.com>
2026-07-08 22:32:52 +0630

    Fix indentation syntax error

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def webhook():
                     
     return Response(status=200)
 
-if __ name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if__name__=='__main__':
+app.run(host='0.0.0.0',port=5000)
